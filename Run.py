569db82b136607d70be19ad7df4dfb28e7189569
jabez007 <jabez.m.007@gmail.com>
2015-12-24 09:42:26 -0600

    Update Formatting and Documentation

--- Run.py
+++ Run.py
@@ -1,21 +1,52 @@
 #!/usr/bin/env python
-__author__ = 'Jimmy'
-#http://www.groovypost.com/howto/microsoft/enable-last-access-time-stamp-to-files-folder-windows-7/
-import sys #http://www.diveintopython.net/scripts_and_streams/command_line_arguments.html
-import os,time
+import os
+import time
 from datetime import datetime
 import subprocess
 import json
 from Tkinter import *
+import tkFont
+
+"""
+*******************************************************************************
+    TITLE:   What To Watch
+	PURPOSE: Search folder and subfolders for unwatched videos,
+        then display the results so the user can select which video(s) to watch
+	AUTHOR:  Jimmy McCann
+	USAGE: WhatToWatch
+	REVISION HISTORY:
+	    *JWM 07/2014 Created
+	    *JWM 12/2015 Cleaned up formatting and adding documentation
+*******************************************************************************
+"""
+
 from BusyManager import BusyManager
 from Splash import Splash
-from dateutil.relativedelta import relativedelta
 
-#VIDEOS = ['.AVI','.FLV','.M4V','.MKV','.MP4','.MPG','.WTV']
-#SKIP = ['SAMPLE','ETRG','RARBG.COM','EXTRATORRENTRG']
-#VIDPROGRAM = "C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe"
 
+"""
+----------
+    NAME:
+    DESCRIPTION:
+    KEYWORDS:
+    CALLED BY: main
+    EXTENDS:
+    ASSUMES:
+    SIDE EFFECTS:
+"""
 class WhatToWatch(Frame):
+
+    """
+    ----------
+        NAME:
+        DESCRIPTION:
+        KEYWORDS:
+        CALLED BY:
+        PARAMETERS:
+        RETURNS:
+        ASSUMES:
+        SIDE EFFECTS:
+    """
     def __init__(self, parent, args):
         Frame.__init__(self, parent)
 
@@ -23,9 +54,20 @@ class WhatToWatch(Frame):
         self.manager = BusyManager(self.parent)
         self.load_configuration()
         self.name_location = {}
-        self.watch_me(args[0]) #SEARCHFOLDER = "\\\\RT-AC68U-1D30\\Libraries\\Videos"
+        self.watch_me(args[0])  # SEARCHFOLDER = "\\\\RT-AC68U-1D30\\Libraries\\Videos"
         self.initUI()
 
+    """
+    ----------
+        NAME:
+        DESCRIPTION:
+        KEYWORDS:
+        CALLED BY:
+        PARAMETERS:
+        RETURNS:
+        ASSUMES:
+        SIDE EFFECTS:
+    """
     def load_configuration(self):
         with open('config') as f:
             data = f.read()
@@ -34,55 +76,113 @@ class WhatToWatch(Frame):
         self.SKIP = jsonDict['skipFiles']
         self.VIDPROGRAM = os.path.join(*jsonDict['videoProgramPath'])
 
+    """
+    ----------
+        NAME:
+        DESCRIPTION:
+        KEYWORDS:
+        CALLED BY:
+        PARAMETERS:
+        RETURNS:
+        ASSUMES:
+        SIDE EFFECTS:
+    """
     def watch_me(self, folder):
-        NOW = datetime.fromtimestamp(time.time())
+        now = datetime.fromtimestamp(time.time())
         self.manager.busy()
-        for root,folders,files in os.walk(folder):
+        for root, folders, files in os.walk(folder):
             for fileName in files:
-                #print ".",
-                name,extension = os.path.splitext(fileName)
+                name, extension = os.path.splitext(fileName)
                 if extension.upper() in self.VIDEOS and not any(s in name.upper() for s in self.SKIP):
-                    filePath = os.path.join(root,fileName)
-                    stats = os.stat(filePath)
+                    filepath = os.path.join(root, fileName)
+                    stats = os.stat(filepath)
                     created = datetime.fromtimestamp(stats.st_ctime)
                     accessed = datetime.fromtimestamp(stats.st_atime)
-                    if relativedelta(accessed,created).days < 1 or relativedelta(NOW,accessed).years > 1:
-                        self.name_location[name] = filePath
+                    if (accessed - created).days <= 1 or (now - accessed).days > 365:
+                        self.name_location[name] = filepath
         self.manager.notbusy()
 
-    def initUI(self):
+    """
+    ----------
+        NAME:
+        DESCRIPTION:
+        KEYWORDS:
+        CALLED BY:
+        PARAMETERS:
+        RETURNS:
+        ASSUMES:
+        SIDE EFFECTS:
+    """
+    def init_ui(self):
         self.parent.title("Downloads to Watch")
+        self.pack(fill=BOTH,
+                  expand=1)
 
-        self.pack(fill=BOTH, expand=1)
+        largefont = tkFont.Font(size=18)
 
-        self.LB = Listbox(self, height=20, width=100)
+        self.LB = Listbox(self,
+                          bd=1,
+                          height=20,
+                          font=largefont)
+        maxwidth = 0
         for movie in self.name_location.keys():
             self.LB.insert(END, movie)
-
-        self.LB.pack(side=LEFT, fill=BOTH)
+            if len(movie) > maxwidth:
+                maxwidth = len(movie)
+        self.LB.config(width=maxwidth)
+        self.LB.pack(side=LEFT,
+                     fill=BOTH,
+                     expand=True)
 
         S = Scrollbar(self)
-        S.pack(side=RIGHT, fill=Y)
+        S.pack(side=RIGHT,
+               fill=Y,
+               expand=1)
         S.config(command=self.LB.yview)
+        
         self.LB.config(yscrollcommand=S.set)
 
-        self.LB.bind("<Double-Button-1>", self.on_double)
+        self.LB.bind("<Double-Button-1>",
+                     self.on_double)
 
+    """
+    ----------
+        NAME:
+        DESCRIPTION:
+        KEYWORDS:
+        CALLED BY:
+        PARAMETERS:
+        RETURNS:
+        ASSUMES:
+        SIDE EFFECTS:
+    """
     def on_double(self, event):
         widget = event.widget
         selection = widget.curselection()
         value = widget.get(selection[0])
-        #print "Selection:",selection,": '%s'" % value
-        p = subprocess.call([self.VIDPROGRAM,self.name_location[value]])
+        p = subprocess.call([self.VIDPROGRAM,
+                             self.name_location[value]])
         self.LB.delete(selection)
 
 
+"""
+----------
+    NAME:
+    DESCRIPTION:
+    KEYWORDS:
+    CALLED BY:
+    PARAMETERS:
+    RETURNS:
+    ASSUMES:
+    SIDE EFFECTS:
+"""
 def main(args):
     root = Tk()
     with Splash(root, 'splashImage.gif', 10.0):
-        WhatToWatch(root,args)
+        WhatToWatch(root,
+                    args)
     root.mainloop()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     if len(sys.argv) > 1:
         main(sys.argv[1:])
